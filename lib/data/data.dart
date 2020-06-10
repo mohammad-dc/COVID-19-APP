129764a8ce1f8157d19ce0fd61dafe1e88d76567
mohammad ahamd <ma3571318z@gmail.com>
2020-06-10 22:26:01 +0300

    1st commit

---
+++ lib/data/data.dart
@@ -0,0 +1,7 @@
+final prevention = [
+  {'assets/distance.png': 'Avoid close\ncontact'},
+  {'assets/wash_hands.png': 'Clean your\nhands often'},
+  {'assets/mask.png': 'Wear a\nfacemask'}
+];
+
+final covidUSADailyNewCases = [12.17, 11.15, 10.02, 11.21, 13.83, 14.16, 14.30];
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:covid19/screens/bottom_nav_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'COVID-19 News',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.white,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: BottomNavScreen(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
